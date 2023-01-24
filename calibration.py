c69f128ee4aacee70716d44adb3cbe71573204ef
winky <dw53067@fer.hr>
2023-01-24 11:56:55 +0100

    Projekt prije uređivanja

--- calibration.py
+++ calibration.py
@@ -1,12 +1,10 @@
-
 import numpy as np 
 import cv2 as cv 
 import glob
 
 ##################### FIND CHESSBOARD CORNERS - objPoints AND imgPoints #################################
 
-chessboardSize = (24,17)
-frameSize = (1440,1080)
+chessboardSize = (8,6)
 
 #Termination criteria 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -19,7 +17,7 @@ objp[:,:2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1,2)
 objPoints = []
 imgPoints = []
 
-images = glob.glob('.png')
+images = glob.glob('images/*.png')
 
 for image in images:
     print(image)
@@ -32,10 +30,11 @@ for image in images:
     if ret == True:
         objPoints.append(objp)
         corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-        imgPoints.append(corners)
+        imgPoints.append(corners2)
 
         #Draw and display the corners
-        cv.dravChessboardCorners(img, chessboardSize, corners2, ret)
+        cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+        
         cv.imshow('img', img)
         cv.waitKey(1000)
 
@@ -43,7 +42,7 @@ cv.destroyAllWindows()
 
 ##################### CALIBRATION #################################
 
-ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objPoints, imgPoints, frameSize, None, None)
+ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objPoints, imgPoints, gray.shape[::-1], None, None)
 
 print("\nCamera Calibrated:", ret)
 print("\nCameraMatrix:\n", cameraMatrix)
@@ -52,7 +51,7 @@ print("\nRotation Vectors:\n", rvecs)
 print("\nTranslation Vectors:\n", tvecs)
 
 #################### UNDISTORTION #################################
-img = cv.imread(SLIKA)
+img = cv.imread('images/image11.png')
 h, w = img.shape[:2]
 newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(cameraMatrix, dist, (w,h), 1, (w,h))
 
@@ -70,7 +69,7 @@ mean_error = 0
 
 for i in range(len(objPoints)):
     imgPoints2, _ = cv.projectPoints(objPoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
-    error = cv.norm(imgPoints[i], imgPoints2, cv.NORM_L/len(imgPoints2))
+    error = cv.norm(imgPoints[i], imgPoints2, cv.NORM_L2)/len(imgPoints2)
     mean_error += error
 
 print("\nTotal error: {}".format(mean_error/len(objPoints)))
